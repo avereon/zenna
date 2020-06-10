cfb7f3a938e1d6339407b9816a39b081ffbad273
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-06-10 15:20:44 -0600

    Convert to rendered image

--- source/main/java/com/avereon/rossa/icon/ZinniaIcon.java
+++ source/main/java/com/avereon/rossa/icon/ZinniaIcon.java
@@ -1,21 +1,29 @@
 package com.avereon.rossa.icon;
 
-import com.avereon.venza.image.ProgramIcon;
+import com.avereon.venza.color.Colors;
+import com.avereon.venza.icon.RenderedIcon;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
-public class ZinniaIcon extends ProgramIcon {
+public class ZinniaIcon extends RenderedIcon {
 
 	private double centerRadius = 3;
 
 	private int likeness = 5;
 
-	private int layers= 2;
+	private int layers = 2;
+
+	public ZinniaIcon() {
+		String pink = Colors.web( Color.HOTPINK );
+		String pingd = Colors.web( Color.HOTPINK.darker() );
+		// This just gets overridden
+		setStyle( "-fx-primary: ladder( -fx-text-background-color, " + pingd + " 50%, " + pink + " 50% );" );
+	}
 
 	@Override
 	protected void render() {
 		double spacing = 360.0 / likeness;
-		double offset = spacing/layers;
+		double offset = spacing / layers;
 
 		for( int layer = 0; layer < layers; layer++ ) {
 			// Lower layer
@@ -40,15 +48,17 @@ public class ZinniaIcon extends ProgramIcon {
 		curveTo( g( 16 + n ), g( 1 ), g( 18 ), g( 8 ), g( 17 ), g( 15 ) );
 		closePath();
 
-		fillAndDraw( Color.HOTPINK, Color.HOTPINK.darker() );
+		fill( getPrimaryPaint() );
+		//draw( Color.HOTPINK.darker() );
 	}
 
 	private void drawCenter() {
 		Paint centerPaint = Color.SADDLEBROWN.darker();
 		setDrawPaint( centerPaint );
 		setFillPaint( centerPaint );
-		fillCenteredOval( g( 16 ), g( 16 ), g( centerRadius ), g( centerRadius ) );
-		drawCenteredOval( g( 16 ), g( 16 ), g( centerRadius ), g( centerRadius ) );
+		startPath();
+		addOval( g( 16 ), g( 16 ), g( centerRadius ), g( centerRadius ) );
+		fill(getSecondaryPaint());
 
 		drawStamen( 10 );
 		drawStamen( 130 );
@@ -58,17 +68,17 @@ public class ZinniaIcon extends ProgramIcon {
 	private void drawStamen( double angleInDegrees ) {
 		double radius = 1.8;
 
-		double x = radius * Math.cos( angleInDegrees * RADIANS_PER_DEGREE );
-		double y = radius * Math.sin( angleInDegrees * RADIANS_PER_DEGREE );
+		double x = radius * Math.cos( Math.toRadians( angleInDegrees ) );
+		double y = radius * Math.sin( Math.toRadians( angleInDegrees ) );
 
-		setDrawPaint( Color.YELLOW );
-		drawDot( g( 16 + x ), g( 16 + y ) );
+		startPath();
+		addOval( g( 16 + x ), g( 16 + y ), g(1), g(1) );
+		fill( Color.YELLOW );
 	}
 
 	public static void main( String[] commands ) {
-		save( new ZinniaIcon(), "target/icons/zinnia.png" );
+		//save( new ZinniaIcon(), "target/icons/zinnia.png" );
 		proof( new ZinniaIcon() );
-		wrapup();
 	}
 
 }
