051083b39b45075ba6e49868658c3b97193ba83b
Mark Soderquist <mark@soderquist.net>
2020-08-25 09:56:30 -0600

    Hexagon icon

--- source/main/java/com/avereon/zenna/icon/HexagonIcon.java
+++ source/main/java/com/avereon/zenna/icon/HexagonIcon.java
@@ -1,18 +1,25 @@
 package com.avereon.zenna.icon;
 
-import com.avereon.zerra.image.SvgIcon;
+import com.avereon.zerra.image.RenderedIcon;
 
-public class HexagonIcon extends SvgIcon {
+public class HexagonIcon extends RenderedIcon {
 
-	public HexagonIcon() {
-		add( generateHexagonCw( 13 ) +" " + generateHexagonCcw( 15 ) );
+	public HexagonIcon() {}
+
+	@Override
+	protected void render() {
+		startPath();
+		getGraphicsContext2D().appendSVGPath( generateHexagon( 13 ) );
+		closePath();
+		setDrawWidth( 2 );
+		draw();
 	}
 
 	public static void main( String[] commands ) {
 		proof( new HexagonIcon() );
 	}
 
-	private String generateHexagonCcw( double r ) {
+	private String generateHexagon( double r ) {
 		double c = 16;
 		double x = r * Math.cos( Math.toRadians( 30 ) );
 		double y = r * Math.sin( Math.toRadians( 30 ) );
@@ -24,25 +31,8 @@ public class HexagonIcon extends SvgIcon {
 		path += " " + c + "," + (c + r);
 		path += " " + (c + x) + "," + (c + y);
 		path += " Z";
-		add( path );
+
 		return path;
 	}
 
-
-	private String generateHexagonCw( double r ) {
-		double c = 16;
-		double x = r * Math.cos( Math.toRadians( 30 ) );
-		double y = r * Math.sin( Math.toRadians( 30 ) );
-
-		String path = "M" + (c + x) + "," + (c - y);
-		path += " " + (c + x) + "," + (c + y);
-		path += " " + c + "," + (c + r);
-		path += " " + (c - x) + "," + (c + y);
-		path += " " + (c - x) + "," + (c - y);
-		path += " " + c + "," + (c - r);
-
-		path += " Z";
-		add( path );
-		return path;
-	}
 }
