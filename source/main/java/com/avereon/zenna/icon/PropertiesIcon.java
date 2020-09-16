a3954366ea2a76f723c4d2458af326d70e56a453
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-09-16 13:34:24 -0600

    Enhance properties icon

--- source/main/java/com/avereon/zenna/icon/PropertiesIcon.java
+++ source/main/java/com/avereon/zenna/icon/PropertiesIcon.java
@@ -1,6 +1,35 @@
 package com.avereon.zenna.icon;
 
-public class PropertiesIcon extends HamburgerIcon {
+import com.avereon.zerra.image.RenderedIcon;
+
+public class PropertiesIcon extends RenderedIcon {
+
+	private static final double r1 = 8;
+
+	private static final double r2 = 16;
+
+	private static final double r3 = 24;
+
+	private static final double e = 6;
+
+	private static final double gap = 6;
+
+	private static final double w = 26;
+
+	@Override
+	protected void render() {
+		startPath();
+		addRow( r1 );
+		addRow( r2 );
+		addRow( r3 );
+		closePath();
+		draw();
+	}
+
+	private void addRow( double y ) {
+		addLine( g( e ), g( y ), g( e ), g( y ) );
+		addLine( g( e + gap ), g( y ), g( w ), g( y ) );
+	}
 
 	public static void main( String[] commands ) {
 		proof( new PropertiesIcon() );
