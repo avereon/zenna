f4b02300c0fd6eff2160fedefe647432cb865ce7
Mark Soderquist <mark@soderquist.net>
2020-11-14 20:37:23 -0700

    Mountain icon

---
+++ source/main/java/com/avereon/zenna/icon/MountainIcon.java
@@ -0,0 +1,28 @@
+package com.avereon.zenna.icon;
+
+import com.avereon.zerra.image.SvgIcon;
+import com.avereon.zerra.image.VectorImage;
+import com.avereon.zerra.image.VectorImageWriter;
+import javafx.application.Platform;
+
+import java.nio.file.Path;
+
+public class MountainIcon extends SvgIcon {
+
+	public MountainIcon() {
+		double offset = Math.sqrt( 0.5 );
+		fill( circle( 16, 16, 15 ) + "M 14,8 l 5,5 -7,9 10,-10 " + (6 + offset) + " " + (6 + offset) + " A 13 13 0 1 0 " + (4 - offset) + "," + (18 + offset) );
+	}
+
+	public static void main( String[] parameters ) {
+		//proof( new MountainIcon() );
+		try {
+			new VectorImageWriter().save( (VectorImage)new MountainIcon().resize( 2048 ), Path.of( "target/mountain.png" ) );
+		} catch( Exception e ) {
+			e.printStackTrace();
+		} finally {
+			Platform.exit();
+		}
+	}
+
+}
