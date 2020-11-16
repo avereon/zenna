58bf7bb958c4a2793fb6595e7fc680120cf541c7
msoderquist <msoderquist@mastercontrol.com>
2020-11-16 08:08:28 -0700

    Mountain icon updates

--- source/main/java/com/avereon/zenna/icon/MountainIcon.java
+++ source/main/java/com/avereon/zenna/icon/MountainIcon.java
@@ -1,23 +1,48 @@
 package com.avereon.zenna.icon;
 
 import com.avereon.zerra.image.SvgIcon;
+import com.avereon.zerra.image.VectorImageWriter;
+import javafx.application.Platform;
+import javafx.scene.paint.Color;
+
+import java.nio.file.Path;
 
 public class MountainIcon extends SvgIcon {
 
 	public MountainIcon() {
+		this( null );
+	}
+
+	public MountainIcon( Color color ) {
+		if( color == null ) {
+			fill( mountain() );
+		} else {
+			fill( mountain(), color );
+		}
+	}
+
+	private String mountain() {
 		double offset = Math.sqrt( 0.5 );
-		fill( circle( 16, 16, 15 ) + "M 14,8 l 5,5 -7,9 10,-10 " + (6 + offset) + " " + (6 + offset) + " A 13 13 0 1 0 " + (4 - offset) + "," + (18 + offset) );
+		return circle( 16, 16, 15 ) + "M 14,8 l 5,5 -7,9 10,-10 " + (6 + offset) + " " + (6 + offset) + " A 13 13 0 1 0 " + (4 - offset) + "," + (18 + offset);
 	}
 
 	public static void main( String[] parameters ) {
-		proof( new MountainIcon() );
-		//		try {
-		//			new VectorImageWriter().save( (VectorImage)new MountainIcon().resize( 2048 ), Path.of( "target/mountain.png" ) );
-		//		} catch( Exception e ) {
-		//			e.printStackTrace();
-		//		} finally {
-		//			Platform.exit();
-		//		}
+		//proof( new MountainIcon() );
+
+		try {
+			save( 2048, null, "mountain.png" );
+			save( 2048, "#80808080", "mountain-background.png" );
+		} catch( Exception e ) {
+			e.printStackTrace();
+		} finally {
+			Platform.exit();
+		}
+	}
+
+	private static void save( int size, String color, String name ) throws Exception {
+		MountainIcon icon = new MountainIcon();
+		if( color != null ) icon = new MountainIcon( Color.web( color ) );
+		new VectorImageWriter().save( (MountainIcon)icon.resize( size ), Path.of( System.getProperty( "user.home" ) + "/Profile/etc/icons/" + name ) );
 	}
 
 }
