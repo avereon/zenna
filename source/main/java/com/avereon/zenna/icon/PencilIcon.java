0852f67f9be01685802fdc1cf1d6e91258018c00
Mark Soderquist <mark@soderquist.net>
2023-03-21 22:06:18 -0600

    Add pencil icon

--- source/main/java/com/avereon/zenna/icon/PencilIcon.java
+++ source/main/java/com/avereon/zenna/icon/PencilIcon.java
@@ -10,7 +10,7 @@ public class PencilIcon extends SvgIcon {
 		double h = 0.5 * w;
 
 		fill( "M26.5,2.5 A" + w + "," + h + ",45,1,1,29.5,5.5 L27.5,7.5 L24.5,4.5 Z" );
-		fill( "M23.5,5.5 L26.5,8.5 L9.5,25.5 L6.5,22.5 Z" );
+		fill( "M22.5,6.5 L25.5,9.5 L10.5,24.5 L7.5,21.5 Z" );
 		fill( "M5.5,23.5 L8.5,26.5 L2,30 Z" );
 	}
 
