c32b44ea581d2f76e9f9f97f2582e6408ad2d621
Mark Soderquist <mark@soderquist.net>
2023-03-21 21:59:23 -0600

    Add pencil icon

--- source/main/java/com/avereon/zenna/icon/PencilIcon.java
+++ source/main/java/com/avereon/zenna/icon/PencilIcon.java
@@ -1,14 +1,17 @@
 package com.avereon.zenna.icon;
 
-public class PencilIcon extends SymbolIcon {
+import com.avereon.zarra.image.SvgIcon;
 
-	@Override
-	protected void render() {
-		startPath();
-		addLine( g( 12 ), g( 16 - L ), g( 12 ), g( 16 + L ) );
-		addLine( g( 20 ), g( 16 - L ), g( 20 ), g( 16 + L ) );
-		closePath();
-		draw();
+public class PencilIcon extends SvgIcon {
+
+	public PencilIcon() {
+		double width = 3;
+		double w = width * 0.5 * Math.sqrt( 2 );
+		double h = 0.5 * w;
+
+		fill( "M26.5,2.5 A" + w + "," + h + ",45,1,1,29.5,5.5 L27.5,7.5 L24.5,4.5 Z" );
+		fill( "M23.5,5.5 L26.5,8.5 L9.5,25.5 L6.5,22.5 Z" );
+		fill( "M5.5,23.5 L8.5,26.5 L2,30 Z" );
 	}
 
 	public static void main( String[] commands ) {
