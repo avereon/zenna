623f9dfc176eb6b8dbe9972a91089d3b2e337761
msoderquist <msoderquist@mastercontrol.com>
2023-06-05 16:45:17 -0600

    Add bell icon

--- source/main/java/com/avereon/zenna/icon/BellIcon.java
+++ source/main/java/com/avereon/zenna/icon/BellIcon.java
@@ -1,18 +1,26 @@
 package com.avereon.zenna.icon;
 
 import com.avereon.zarra.image.SvgIcon;
+import javafx.scene.paint.Color;
 
 public class BellIcon extends SvgIcon {
 
-    public BellIcon() {
-        // Bell
-        fill( "M4,24C4,18,8,20,8,10A5,5,0,0,1,24,10C24,20,28,18,28,24");
+	public BellIcon() {
+		this( null );
+	}
 
-        // Gong
-        fill("M13,26L19,26A3,3,0,0,1,13,26");
-    }
+	public BellIcon( Color color ) {
+		setStrokePaint( color );
 
-    public static void main( String[] commands ) {
-        proof( new BellIcon() );
-    }
- }
+		// Bell
+		fill( "M4,24C4,18,8,20,8,10A5,5,0,0,1,24,10C24,20,28,18,28,24" );
+
+		// Gong
+		fill( "M13,26L19,26A3,3,0,0,1,13,26" );
+	}
+
+	public static void main( String[] commands ) {
+		proof( new BellIcon() );
+	}
+
+}
