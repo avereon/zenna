60b4977ad2552cb70c4104cf0e1825e8da5b3c37
Mark Soderquist <mark@soderquist.net>
2023-06-11 22:10:54 -0600

    Add vibrate to bell icon

--- source/main/java/com/avereon/zenna/icon/BellIcon.java
+++ source/main/java/com/avereon/zenna/icon/BellIcon.java
@@ -5,22 +5,36 @@ import javafx.scene.paint.Color;
 
 public class BellIcon extends SvgIcon {
 
-	public BellIcon() {
-		this( null );
-	}
+    public BellIcon() {
+        this(null);
+    }
 
-	public BellIcon( Color color ) {
-		setStrokePaint( color );
+    public BellIcon(Color color) {
+        this(color, false);
+    }
 
-		// Bell
-		fill( "M4,24C4,18,8,20,8,10A5,5,0,0,1,24,10C24,20,28,18,28,24" );
+    public BellIcon(boolean vibrate) {
+        this(null, vibrate);
+    }
 
-		// Gong
-		fill( "M13,26L19,26A3,3,0,0,1,13,26" );
-	}
+    public BellIcon(Color color, boolean vibrate) {
+        setStrokePaint(color);
 
-	public static void main( String[] commands ) {
-		proof( new BellIcon() );
-	}
+        // Bell
+        fill("M4,24C4,18,8,20,8,10A5,5,0,0,1,24,10C24,20,28,18,28,24");
+
+        // Gong
+        fill("M13,26L19,26A3,3,0,0,1,13,26");
+
+        // Vibrate
+        if (vibrate) {
+            draw("M4,12 A14,14,0,0,1,6,4", 0.75 * DEFAULT_STROKE_WIDTH);
+            draw("M28,12 A14,14,0,0,0,26,4", 0.75 * DEFAULT_STROKE_WIDTH);
+        }
+    }
+
+    public static void main(String[] commands) {
+        proof(new BellIcon(true));
+    }
 
 }
