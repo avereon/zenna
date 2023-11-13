2f5c57537fec73289a9a9bf6c3377aa1113cf95e
msoderquist <msoderquist@mastercontrol.com>
2023-11-13 09:30:16 -0700

    Update welcome icon

--- source/main/java/com/avereon/zenna/icon/WelcomeIcon.java
+++ source/main/java/com/avereon/zenna/icon/WelcomeIcon.java
@@ -1,30 +1,16 @@
 package com.avereon.zenna.icon;
 
-import com.avereon.zarra.image.RenderedIcon;
+import com.avereon.zarra.image.SvgIcon;
 
-public class WelcomeIcon extends RenderedIcon {
+public class WelcomeIcon extends SvgIcon {
 
-	@Override
-	protected void render() {
-		// TODO Be a bit more creative
-		// Maybe a "waving hand" or "open door"
-		startPath();
-		moveTo( g( 2 ), g( 4 ) );
-		lineTo( g( 2 ), g( 28 ) );
-		lineTo( g( 30 ), g( 28 ) );
-		lineTo( g( 30 ), g( 4 ) );
-		closePath();
-		moveTo( g( 4 ), g( 6 ) );
-		lineTo( g( 4 ), g( 26 ) );
-		lineTo( g( 28 ), g( 26 ) );
-		lineTo( g( 28 ), g( 6 ) );
-		closePath();
-		moveTo( g( 6 ), g( 8 ) );
-		lineTo( g( 6 ), g( 24 ) );
-		lineTo( g( 26 ), g( 24 ) );
-		lineTo( g( 26 ), g( 8 ) );
-		closePath();
-		fill();
+	public WelcomeIcon() {
+		String tl = arc( 6, 6, 2, -90, -90 );
+		String bl = arc( 6, 26, 2, -180, -90 );
+		String br = arc( 26, 26, 2, -270, -90 );
+		String tr = arc( 26, 6, 2, 0, -90 );
+		String icon = "M6,4 " + tl + " L4,26 " + bl + " L26,28 " + br + " L28,6 " + tr + " Z";
+		fill( icon );
 	}
 
 	public static void main( String[] commands ) {
