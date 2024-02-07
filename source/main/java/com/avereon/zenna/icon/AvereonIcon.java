bc106923bb5449ca1d5b84c8ca669621954cf59e
msoderquist <msoderquist@mastercontrol.com>
2024-02-06 19:41:23 -0700

    Add new AvereonIcon class
    A new class named AvereonIcon has been added under the "com.avereon.zenna.icon" package. This class extends the WingDiscLargeIcon class and a proof method is invoked in the main method with specific color and null as parameters.

---
+++ source/main/java/com/avereon/zenna/icon/AvereonIcon.java
@@ -0,0 +1,12 @@
+package com.avereon.zenna.icon;
+
+import com.avereon.zarra.image.Proof;
+import javafx.scene.paint.Color;
+
+public class AvereonIcon extends WingDiscLargeIcon{
+
+	public static void main( String[] commands ) {
+		Proof.proof( new AvereonIcon(), Color.web( "#206080" ), null );
+	}
+
+}
